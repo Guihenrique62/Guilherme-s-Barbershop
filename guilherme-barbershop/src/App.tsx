cbb9c5fda56708b546b8c881bef8b1388a3d5421
guihenrique62 <guilhermeportosantos1@gmail.com>
2024-07-04 09:30:10 -0300

    Feat - HTML components

--- guilherme-barbershop/src/App.tsx
+++ guilherme-barbershop/src/App.tsx
@@ -1,33 +1,118 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+      <header>
+        <div>
+          <img src="" alt="" />
+        </div>
+
+        <div>
+          <div>
+            <a href="">HOME</a>
+            <a href="">SOBRE</a>
+            <a href="">EQUIPE</a>
+            <a href="">SERVIÇOS</a>
+            <a href="">CLUBE</a>
+          </div>
+
+          <button>
+            AGENDE AGORA
+          </button>
+        </div>
+      </header>
+
+      {/* Component Home */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Seu Estilo em boas mãos.</h1>
+        <h3>Espaço personalizado e climatizado para te oferecer uma experiência sem igual.</h3>
+        <button>AGENDE SEU HORÁRIO</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+      {/* Component Sobre */}
+      <div>
+        <div>
+          <h2>Sobre</h2>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>Com uma pegada mais moderna, a Guilherme´s Barbearia vai na contramão do tradicional estilo das barbearias e aposta na originalidade.</p>
+          <p>Localizada em um dos bairros mais conhecidos e badalados de Goiânia.</p>
+          <p>Desde 2024, buscando pelo bem estar de todos os clientes através dos serviços de qualidade e com os melhores profissionais.</p>
+        </div>
+
+        <button>
+          SAIBA MAIS
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+      {/* Component Equipe */}
+
+      <div>
+        <h2>Conheça nossa equipe</h2>
+        <p>Um time de feras, cada qual com o seu estilo, com seu jeito particular.Mas todos com muita bagagem e capazes de cuidar do seu visual de forma única, um atendimento personalizado.</p>
+        
+        <div>
+          <div>
+            <img src="" alt="" />
+            <span>Nome</span>
+          </div>
+
+        </div>
+        
+      </div>
+
+      <div>
+        <h2>Nossos serviços</h2>
+
+        <div>
+          <div>
+            <p>Barba</p>
+            <span>R$ 55,00</span>
+          </div>
+
+          <div>
+            <p>Barba</p>
+            <span>R$ 55,00</span>
+          </div>
+
+        </div>
+
+        <button>AGENDE SEU HORARIO</button>
+      </div>
+
+
+
+
+
+      <footer>
+        <div>
+          <img src="" alt="" />
+          <span>Ico</span>
+          <span>Ico</span>
+          <span>Ico</span>
+
+        </div>
+
+        <div>
+          <p>Funcionamento</p>
+          <p>Segunda a Sexta - 09h às 20h</p>
+          <p>Sábado - 08h às 18h</p>
+        </div>
+
+        <div>
+          <p>Localização</p>
+          <p>Av. Ipanema - Jd Atlântico - Goiânia GO</p>
+          <p>{"Tel. (62) 98643-7924"}</p>
+        </div>
+      </footer>
+
     </>
   )
 }
