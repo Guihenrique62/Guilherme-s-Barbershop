46ce1ad085d5ff8346f8016ba69523d4406f5c71
Guilherme Henrique <guilhermeportosantos1@gmail.com>
2024-07-07 15:34:49 -0300

    feat - image container

--- barbershop/src/App.tsx
+++ barbershop/src/App.tsx
@@ -26,19 +26,39 @@ function App() {
 
       {/* Component Equipe */}
 
-      <div className='bg-black text-white '>
-        <h2 className='text-5xl roboto-bold'>Conheça nossa equipe</h2>
-        <p>Um time de feras, cada qual com o seu estilo, com seu jeito particular.Mas todos com muita bagagem e capazes de cuidar do seu visual de forma única, um atendimento personalizado.</p>
-        
-        <div>
-          <div>
-            <img src="" alt="" />
-            <span>Nome</span>
-          </div>
+      <div className='flex bg-black items-center justify-center '>
+        <div className='flex flex-col text-white self-center container gap-5'>
+          <h2 className='text-5xl roboto-bold'>Conheça nossa equipe</h2>
+          <p>Um time de feras, cada qual com o seu estilo, com seu jeito particular. Mas todos com muita bagagem e capazes de cuidar do seu visual de forma única, um atendimento personalizado.</p>
 
+          <div className='flex gap-4 items-center justify-center bg-slate-400 p-20 w-full h-auto'>
+            <div className='flex flex-row gap-4 items-center justify-center'>
+
+              <div className='flex flex-col items-center bg-white p-2'>
+                <img src="pic1.jpg" alt="picture" className='h-20 w-20 object-cover'/>
+                <span>Guilherme</span>
+              </div>
+
+              <div className='flex flex-col items-center bg-white p-2'>
+                <img src="pic1.jpg" alt="picture" className='h-20 w-20 object-cover'/>
+                <span className='bg-zinc-400/30 text-black'>Guilherme</span>
+              </div>
+
+              <div className='flex flex-col items-center bg-white p-2'>
+                <img src="pic1.jpg" alt="picture" className='h-20 w-20 object-cover'/>
+                <span>Guilherme</span>
+              </div>
+
+              <div className='flex flex-col items-center bg-white p-2'>
+                <img src="pic1.jpg" alt="picture" className='h-20 w-20 object-cover'/>
+                <span>Guilherme</span>
+              </div>
+
+            </div>
+          </div>
         </div>
-        
       </div>
+
 
       <div>
         <h2>Nossos serviços</h2>
